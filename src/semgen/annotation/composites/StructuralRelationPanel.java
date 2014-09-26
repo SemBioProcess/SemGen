a27b711282e3843932abb91032f98fbf8065a1e0
Ryan James <rcjames1004@gmail.com>
2014-09-26 08:02:49 -0700

    Remove "contianed_in" as an annotation option
    On 9/19/2014 in our weekly SemGen / VPR meeting decided to remove
    "contained_in" as an annotation descriptor option and leave "part_of" as
    the label describing the relationship between an entity and its parent.
    
    In these changes I removed the combobox containing "contained_in" and
    "part_of" and in its place inserted a label displaying "part_of".

--- src/semgen/annotation/composites/StructuralRelationPanel.java
+++ src/semgen/annotation/composites/StructuralRelationPanel.java
@@ -1,10 +1,13 @@
 package semgen.annotation.composites;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Box;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.semanticweb.owlapi.model.OWLException;
@@ -13,14 +16,13 @@ import semgen.SemGenGUI;
 import semgen.annotation.AnnotationDialog;
 import semsim.model.annotation.StructuralRelation;
 
-public class StructuralRelationPanel extends JPanel implements ActionListener{
+public class StructuralRelationPanel extends JPanel{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 4319031091828000135L;
 	public AnnotationDialog anndialog;
-	public StructuralRelationComboBox srcb;
 	public StructuralRelation structuralRelation;
 
 
@@ -28,22 +30,13 @@ public class StructuralRelationPanel extends JPanel implements ActionListener{
 		this.anndialog = anndialog;
 		structuralRelation = relation;
 		setLayout(new BorderLayout());
-		srcb = new StructuralRelationComboBox(relation);
-		srcb.addActionListener(this);
-		add(srcb, BorderLayout.WEST);
+		
+		JLabel partOf = new JLabel("part_of");
+		partOf.setFont(new Font("SansSerif", Font.ITALIC, SemGenGUI.defaultfontsize));
+		partOf.setPreferredSize(new Dimension(135,30));
+		partOf.setMaximumSize(new Dimension(135,30));
+		
+		add(partOf, BorderLayout.WEST);
 		add(Box.createGlue(), BorderLayout.EAST);
-		
-		
-	}
-
-	public void actionPerformed(ActionEvent arg0) {
-		if(arg0.getSource()==srcb){
-			structuralRelation = (StructuralRelation) SemGenGUI.namestructuralrelationmap.get(srcb.getSelectedItem());
-			try {
-				anndialog.updateCompositeAnnotationFromUIComponents();
-			} catch (OWLException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
