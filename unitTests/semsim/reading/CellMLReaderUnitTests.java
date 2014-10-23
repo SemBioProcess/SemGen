8bdfa913a2696b4aa392dcd04e645c25fb20b746
Ryan James <rcjames1004@gmail.com>
2014-10-23 13:18:43 -0700

    Unit tests
    This check in adds the JUnit unit test framework to SemGen. We can now
    add unit tests. I've created a unit test class for CellMLReader that
    contains a default unit test that always passed. Next I plan on writing
    a simple unit test to verify reading cell my files works properly

---
+++ unitTests/semsim/reading/CellMLReaderUnitTests.java
@@ -0,0 +1,13 @@
+package semsim.reading;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CellMLReaderUnitTests {
+
+	@Test
+	public void testPass() {
+		assertEquals(1,  1);
+	}
+}
