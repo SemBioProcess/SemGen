0335a0b3cf585669913028105e47c75647fe621c
Chris <thompsct54701@gmail.com>
2014-11-17 11:07:27 -0600

    Fixed bug that was preventing the Composite Annotation Component Search Dialog from opening.

--- src/semgen/annotation/composites/CompositeAnnotationComponentSearchDialog.java
+++ src/semgen/annotation/composites/CompositeAnnotationComponentSearchDialog.java
@@ -36,73 +36,75 @@ public class CompositeAnnotationComponentSearchDialog extends AddReferenceClassD
 	
 	
 	public void propertyChange(PropertyChangeEvent arg0) {
-		String value = optionPane.getValue().toString();
-		if(value == "Apply" && this.getFocusOwner() != refclasspanel.findbox){
-			
-			// If something from list actually selected
-			if(refclasspanel.resultslistright.getSelectedValue()!=null){
-				String desc = (String) refclasspanel.resultslistright.getSelectedValue();
-				URI uri = URI.create(refclasspanel.resultsanduris.get(refclasspanel.resultslistright.getSelectedValue()));
+		if (arg0.getPropertyName()=="value") {
+			String value = optionPane.getValue().toString();
+			if(value == "Apply" && this.getFocusOwner() != refclasspanel.findbox){
 				
-				// If we're annotating a physical property...
-				if(pmcPanel.smc instanceof PhysicalProperty){
-					if(pmcPanel.checkOPBpropertyValidity(uri)){
-						pmcPanel.smc.removeAllReferenceAnnotations();
-						pmcPanel.smc.addReferenceOntologyAnnotation(SemSimConstants.REFERS_TO_RELATION, uri, desc);
+				// If something from list actually selected
+				if(refclasspanel.resultslistright.getSelectedValue()!=null){
+					String desc = (String) refclasspanel.resultslistright.getSelectedValue();
+					URI uri = URI.create(refclasspanel.resultsanduris.get(refclasspanel.resultslistright.getSelectedValue()));
+					
+					// If we're annotating a physical property...
+					if(pmcPanel.smc instanceof PhysicalProperty){
+						if(pmcPanel.checkOPBpropertyValidity(uri)){
+							pmcPanel.smc.removeAllReferenceAnnotations();
+							pmcPanel.smc.addReferenceOntologyAnnotation(SemSimConstants.REFERS_TO_RELATION, uri, desc);
+						}
+						else{
+							pmcPanel.showInvalidOPBpropertyError();
+							return;
+						}
 					}
-					else{
-						pmcPanel.showInvalidOPBpropertyError();
-						return;
+					//Otherwise, if the reference term hasn't been added to the model yet...
+					else if(pmcPanel.semsimmodel.getPhysicalModelComponentByReferenceURI(uri)==null){
+						if(pmcPanel.smc instanceof PhysicalProcess){
+							pmcPanel.smc = pmcPanel.semsimmodel.addReferencePhysicalProcess(uri, desc);
+						}
+						else if(pmcPanel.smc instanceof PhysicalEntity){
+							pmcPanel.smc = pmcPanel.semsimmodel.addReferencePhysicalEntity(uri, desc);
+							
+							// If we are using an FMA term, store the numerical version of the ID
+							String altID = null;
+							((ReferencePhysicalEntity)pmcPanel.smc).getFirstRefersToReferenceOntologyAnnotation().setAltNumericalID(altID);
+						}
 					}
-				}
-				//Otherwise, if the reference term hasn't been added to the model yet...
-				else if(pmcPanel.semsimmodel.getPhysicalModelComponentByReferenceURI(uri)==null){
-					if(pmcPanel.smc instanceof PhysicalProcess){
-						pmcPanel.smc = pmcPanel.semsimmodel.addReferencePhysicalProcess(uri, desc);
+					// Otherwise reuse existing annotation
+					else pmcPanel.smc = pmcPanel.semsimmodel.getPhysicalModelComponentByReferenceURI(uri);
+					
+					// Refresh the annotation based on the PhysicalModelComponents specified in the PhysicalModelComponentPanels
+					try {
+						pmcPanel.anndialog.updateCompositeAnnotationFromUIComponents();
+					} catch (OWLException e) {
+						e.printStackTrace();
 					}
-					else if(pmcPanel.smc instanceof PhysicalEntity){
-						pmcPanel.smc = pmcPanel.semsimmodel.addReferencePhysicalEntity(uri, desc);
-						
-						// If we are using an FMA term, store the numerical version of the ID
-						String altID = null;
-						((ReferencePhysicalEntity)pmcPanel.smc).getFirstRefersToReferenceOntologyAnnotation().setAltNumericalID(altID);
+					
+					pmcPanel.anndialog.compositepanel.setAddButtonsEnabled();
+					
+					// Refresh all the comboboxes in the composite annotation interface
+					for(Component c : pmcPanel.anndialog.compositepanel.getComponents()){
+						if(c instanceof SemSimComponentAnnotationPanel){
+							((SemSimComponentAnnotationPanel)c).refreshComboBoxItemsAndButtonVisibility();
+						}
 					}
-				}
-				// Otherwise reuse existing annotation
-				else pmcPanel.smc = pmcPanel.semsimmodel.getPhysicalModelComponentByReferenceURI(uri);
-				
-				// Refresh the annotation based on the PhysicalModelComponents specified in the PhysicalModelComponentPanels
-				try {
-					pmcPanel.anndialog.updateCompositeAnnotationFromUIComponents();
-				} catch (OWLException e) {
-					e.printStackTrace();
-				}
-				
-				pmcPanel.anndialog.compositepanel.setAddButtonsEnabled();
-				
-				// Refresh all the comboboxes in the composite annotation interface
-				for(Component c : pmcPanel.anndialog.compositepanel.getComponents()){
-					if(c instanceof SemSimComponentAnnotationPanel){
-						((SemSimComponentAnnotationPanel)c).refreshComboBoxItemsAndButtonVisibility();
+					// Refresh combobox items in singular annotation interface
+					pmcPanel.anndialog.singularannpanel.refreshComboBoxItemsAndButtonVisibility();
+					
+					if(pmcPanel.smc.hasRefersToAnnotation()) 
+						pmcPanel.urlbutton.setTermURI(pmcPanel.smc.getFirstRefersToReferenceOntologyAnnotation().getReferenceURI());
+					optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
+					if(refclasspanel.ontologychooser.getComponentCount()>2){
 					}
+					
+					// Refresh the combobox items for the Singular Annotation panel in the AnnotationDialog
+					pmcPanel.anndialog.refreshSingularAnnotation();
 				}
-				// Refresh combobox items in singular annotation interface
-				pmcPanel.anndialog.singularannpanel.refreshComboBoxItemsAndButtonVisibility();
-				
-				if(pmcPanel.smc.hasRefersToAnnotation()) 
-					pmcPanel.urlbutton.setTermURI(pmcPanel.smc.getFirstRefersToReferenceOntologyAnnotation().getReferenceURI());
-				optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
-				if(refclasspanel.ontologychooser.getComponentCount()>2){
-				}
-				
-				// Refresh the combobox items for the Singular Annotation panel in the AnnotationDialog
-				pmcPanel.anndialog.refreshSingularAnnotation();
 			}
+			else if (value == "Cancel") {
+				refclasspanel.querythread.stop();
+				optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
+			}
+			dispose();
 		}
-		else if (value == "Cancel") {
-			refclasspanel.querythread.stop();
-			optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
-		}
-		dispose();
 	}
 }
