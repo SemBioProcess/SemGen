0d9507a65e7294fe715d8f54e18d45c186c485c9
Ryan James <rcjames1004@gmail.com>
2015-02-01 21:29:39 -0800

    Add mock.js
    Mock out java side of things so we can run quick manual tests in a
    standalone browser.

---
+++ src/resources/js/mock.js
@@ -0,0 +1,40 @@
+/**
+ * If we're loading outside of SemGen create a mock sender and receiver
+ */
+$(window).load(function() {
+	// sendNSCommand is defined when the stage is loaded in SemGen
+	if(typeof sendNSCommand != 'undefined')
+		return;
+	
+	var mockSender = {
+			addModel: function() {
+				mockReceiver.addModel("Test model");
+			},
+	};
+	
+	var mockReceiver = {
+			onAddModel: function (handler) { this.addModel = handler; },
+	};
+	
+	var event; // The custom event that will be created
+
+	if (document.createEvent) {
+		event = document.createEvent("HTMLEvents");
+		event.initEvent("cwb-initialized", true, true);
+	}
+	else {
+		event = document.createEventObject();
+		event.eventType = "cwb-initialized";
+	}
+
+	event.eventName = "cwb-initialized";
+	event.commandReceiver = mockReceiver;
+	event.commandSender = mockSender;
+
+	if (document.createEvent) {
+		window.dispatchEvent(event);
+	}
+	else {
+		window.fireEvent("on" + event.eventType, event);
+	}
+});
