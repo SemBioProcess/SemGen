963f370e4fcb41141108f1e93e23da1c406ffdd5
Ryan James <rcjames1004@gmail.com>
2015-02-05 15:12:04 -0800

    Add flyout to stage
    Still need to hook it up to model elements

--- src/resources/js/stage.js
+++ src/resources/js/stage.js
@@ -3,6 +3,7 @@ $(window).bind("cwb-initialized", function(e) {
 	var sender = e.originalEvent.commandSender;
 
 	var graph = new Graph();
+	var flyoutMenu = createFlyoutMenu();
 	
 	$(".addModelButton").click(function() {
 		sender.addModel();
@@ -12,3 +13,64 @@ $(window).bind("cwb-initialized", function(e) {
 		graph.addNode(modelName);
 	})
 });
+
+/**
+ * Creates a flyout menu for the stage
+ */
+function createFlyoutMenu () {
+	var flyoutMenu = new FlyoutMenu();
+	
+	var createComingSoonClickHandler = function (taskName) {
+		return function () { alert("'" + taskName + "' coming soon!") };
+	}
+	
+	// Define menu items
+	var menuItems = [
+		{
+			text: "Merge",
+			onClick: createComingSoonClickHandler("Merge"),
+		},
+		{
+			text: "Annotate",
+			onClick: createComingSoonClickHandler("Annotate"),
+		},
+		{
+			text: "Extract",
+			onClick: createComingSoonClickHandler("Extract"),
+		},
+		{
+			text: "Visualize",
+			onClick: createComingSoonClickHandler("Visualize"),
+		},
+	];
+	
+	// Add each menu item to the flyout
+	menuItems.forEach(function (item) {
+		flyoutMenu.addMenuItem(item);
+	});
+}
+
+/**
+ * Flyout style menu
+ */
+function FlyoutMenu() {
+	
+	// Add an item to the menu
+	this.addMenuItem = function (item) {
+		var listItem = document.createElement("li");
+		listItem.innerHTML = item.text;
+		$(listItem).click(item.onClick);
+		root.appendChild(listItem);
+	};
+	
+	// Hide the flyout
+	this.getRoot = function () {
+		return $(root)
+	}
+	
+	// Create the root element
+	var root = document.createElement("ul");
+	root.className = "flyoutMenu";
+	this.getRoot().hide();
+	document.body.appendChild(root);
+}
