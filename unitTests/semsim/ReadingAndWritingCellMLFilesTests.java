0a39a97e9ffc60d885e3abfd31b6e7e0ed335afa
Ryan James <rcjames1004@gmail.com>
2015-04-14 23:50:09 -0700

    Reading and writing CellML files tests
    This is the initial check in for the set of tests that will test the
    end-end reading and writing cellml files. Unit testing the cellml reader
    and writer is too expensive (we don't need this to be production
    quality) so we're creating end-end tests to test everything at once in
    one swoop.

---
+++ unitTests/semsim/ReadingAndWritingCellMLFilesTests.java
@@ -0,0 +1,13 @@
+package semsim;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReadingAndWritingCellMLFilesTests {
+
+	@Test
+	public void dummyTest_DoNothing_AssertTrue() {
+		assertTrue(true);
+	}
+}
