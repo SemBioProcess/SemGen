711c562bfcc823f340dbd6a2fb1c08bacc45c451
Ryan James <rcjames1004@gmail.com>
2015-05-13 11:37:05 -0700

    Only log severe messages from JxBrowser
    There are too many JxBrowser log message! These changes force the
    browser to only log severe messages. There are still a bunch of red
    messages that are logged when the browser starts up, but it's a big
    improvement compared to what was logged before this change.

--- src/semgen/visualizations/SemGenCommunicatingWebBrowser.java
+++ src/semgen/visualizations/SemGenCommunicatingWebBrowser.java
@@ -1,9 +1,10 @@
 package semgen.visualizations;
 
 import javax.naming.InvalidNameException;
+import com.teamdev.jxbrowser.chromium.LoggerProvider;
+import java.util.logging.*;
 
-public class SemGenCommunicatingWebBrowser extends
-		CommunicatingWebBrowser<SemGenWebBrowserCommandSender> {
+public class SemGenCommunicatingWebBrowser extends CommunicatingWebBrowser<SemGenWebBrowserCommandSender> {
 	private static final long serialVersionUID = 1L;
 	// Stage html
 	private final static String StageHtml = "/resources/stage.html";
@@ -17,6 +18,10 @@ public class SemGenCommunicatingWebBrowser extends
 	    this.loadURL(this.getClass().getResource(StageHtml).toString() + "?testMode=false");
 	    
 	    System.out.println("SemGen web browser loaded");
+	    
+	    LoggerProvider.getBrowserLogger().setLevel(Level.SEVERE);			// The BrowserLogger is used to log browser messages.
+	    LoggerProvider.getIPCLogger().setLevel(Level.SEVERE);				// The IPCLogger is used to log IPC (Inter-Process Communication) messages.
+	    LoggerProvider.getChromiumProcessLogger().setLevel(Level.SEVERE);	// The ChromiumProcessLogger is used to log messages that are come from Chromium process.
 	}
 
 }
