a26b53324e578896ec9fc2a1aaa59e6dfed1a07c
Graham Kim <karam.kim9@gmail.com>
2015-08-05 10:02:45 -0700

    Display mediator nodes as orange

--- src/resources/js/nodes/physiomapNode.js
+++ src/resources/js/nodes/physiomapNode.js
@@ -5,10 +5,30 @@
 PhysioMapNode.prototype = new Node();
 PhysioMapNode.prototype.constructor = PhysioMapNode;
 function PhysioMapNode (graph, data, parentNode) {
-	Node.prototype.constructor.call(this, graph, data.id, data.name, parentNode, data.inputs, 5, "#1F77B4", 14, "Entity", -300);
+	// Ensure the node type is formatted properly
+	data.nodeType = data.nodeType.toLowerCase().capitalizeFirstLetter();
+
+	// Get the correct group from the node type
+	this.group = physiomapTypeToGroup[data.nodeType];
+	if(this.group == "undefined")
+		throw "Invalid PhysioMap node type: " + data.nodeType;
+
+	Node.prototype.constructor.call(this, graph, data.id, data.name, parentNode, data.inputs, 5, physiomapTypeToColor[data.nodeType], 14, data.nodeType, -300);
 	
 	this.displayName = data.name.replace("Portion of ", "").capitalizeFirstLetter();
 	this.addClassName("physiomapNode");
 	this.addBehavior(HiddenLabelNodeGenerator);
 
 }
+
+// Maps node type to group number
+var physiomapTypeToGroup = {
+	"Entity": 0,
+	"Mediator": 1,
+};
+
+// Maps node type to node color
+var physiomapTypeToColor = {
+	"Entity": "#1F77B4",
+	"Mediator": "#FF7F0E"
+};
