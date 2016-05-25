9c130bad4737a5b425f989b91ab692b9a30e0d4a
Graham Kim <graham.k.kim@gmail.com>
2016-05-24 20:52:09 -0700

    Display blank when DS doesn't have equation and start value

--- src/semgen/merging/workbench/DataStructureDescriptor.java
+++ src/semgen/merging/workbench/DataStructureDescriptor.java
@@ -23,7 +23,9 @@ public class DataStructureDescriptor {
 		
 		if(ds.getComputation().getComputationalCode() != null)
 			descriptormap.put(Descriptor.computationalcode, ds.getComputation().getComputationalCode());
-		else descriptormap.put(Descriptor.computationalcode, ds.getName().substring(ds.getName().lastIndexOf(".") + 1)+"="+ds.getStartValue());
+		else if(ds.getStartValue() != null)
+			descriptormap.put(Descriptor.computationalcode, ds.getName().substring(ds.getName().lastIndexOf(".") + 1)+"="+ds.getStartValue());
+		else descriptormap.put(Descriptor.computationalcode, "");
 
 		if(ds.getCompositeAnnotationAsString(false) != "[unspecified]")
 			descriptormap.put(Descriptor.annotation, ds.getCompositeAnnotationAsString(false));
