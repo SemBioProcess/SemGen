b90d7d83393a2a69dfcbb6d984cf54abfd426f0a
thompsct <thompsct54701@gmail.com>
2016-10-06 15:37:56 -0500

    Reaching a limit on one axis no longer prevents dragging on the other when dragging a hull.

--- cfg/stage/js/nodes/behaviors/parentNodeDrag.js
+++ cfg/stage/js/nodes/behaviors/parentNodeDrag.js
@@ -15,22 +15,28 @@ function parentDrag(parent) {
 	parent.drag.push(function (d) {
 		   // Drag functionality
 		if (d.showchildren) {
+			var dx = 0;
+			var dy = 0;
 			if (
 				(d3.event.dx + d.xmin) > 10
-				&& (d3.event.dx + d.xmax) < d.graph.w-10
-				&& (d3.event.dy + d.ymin) > 10
-				&& (d3.event.dy + d.ymax) < d.graph.h-10
-			){
-				d.globalApply(function(d) {
-					d.setLocation(d.xpos() + d3.event.dx, d.ypos() + d3.event.dy);
-					d.px += d3.event.dx;
-					d.py += d3.event.dy;
-				});
+				&& (d3.event.dx + d.xmax) < d.graph.w-10) {
+					dx = d3.event.dx;
 			}
-			else {
-				d.setLocation(d.xpos(), d.ypos());
-				$(parent).triggerHandler("dragend");
+			
+			if (
+				(d3.event.dy + d.ymin) > 10
+				&& (d3.event.dy + d.ymax) < d.graph.h-10) {
+					dy = d3.event.dy;
 			}
+			d.globalApply(function(n) {
+				n.setLocation(n.xpos() + dx, n.ypos() + dy);
+				n.px += dx;
+				n.py += dy;
+			});
+		
+	
+				//d.setLocation(d.xpos(), d.ypos());
+			//	$(parent).triggerHandler("dragend");
 		}
 		else {
 			if (
