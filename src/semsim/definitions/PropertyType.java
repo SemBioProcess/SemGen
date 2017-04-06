ac9c2f5b035b16e0cbb79bb421d27ad82939e249
Maxwell Neal <maxneal@gmail.com>
2017-04-06 16:41:12 -0700

    Minor documentation added to classes in semsim.definitions.

--- src/semsim/definitions/PropertyType.java
+++ src/semsim/definitions/PropertyType.java
@@ -1,9 +1,24 @@
 package semsim.definitions;
 
+/**
+ * Enumeration of the three main types of physical properties
+ * represented in biosimulation models. These correspond to the main
+ * classes of physical properties in the Ontology of Physics for Biology.
+ * 
+ * - Properties of physical entities ("State properties"). Examples: The chemical
+ * concentration of a molecular species, the fluid volume of a portion of blood.
+ * 
+ * - Properties of physical processes ("Rate properties"). Examples: The rate of
+ * the phosphofructokinase reaction, the rate of blood flow through an artery.
+ * 
+ * - Properties of physical dependencies ("Constitutive properties"). Examples: 
+ * A first order reaction rate constant used to determine a reaction rate,
+ *  the fluid resistance term in an instance of Ohm's Law for fluids. 
+ */
 public enum PropertyType {
-	Unknown ("Constitutive"),
 	PropertyOfPhysicalEntity ("State"),
-	PropertyOfPhysicalProcess ("Rate");
+	PropertyOfPhysicalProcess ("Rate"),
+	Unknown ("Constitutive");
 	
 	// String representation of enum
 	private final String _name;
@@ -12,6 +27,9 @@ public enum PropertyType {
         _name = name;
     }
 
+    /**
+     * Returns the name of the property type as a string.
+     */
     public String toString(){
        return _name;
     }
