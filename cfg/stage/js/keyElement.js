b15f02a3578ec42f16147c92275433e34cadc611
Graham Kim <graham.k.kim@gmail.com>
2017-09-08 12:54:21 -0700

    #247 Keep the positions of the key buttons static

--- cfg/stage/js/keyElement.js
+++ cfg/stage/js/keyElement.js
@@ -1,135 +1,144 @@
 
-function KeyElement (visibleModDepKeys, hiddenModDepKeys, visiblePhysioKeys, hiddenPhysioKeys) {
-	var addedKeys = [];
-	
-	this.initialize = function (graph) {
-		$(graph).on("postupdate", function () {
-			addedKeys = [];
-			$("#stagemenu #stagekey #modelkey").empty();
-			visibleModDepKeys.empty();
-			hiddenModDepKeys.empty();
-			visiblePhysioKeys.empty();
-			hiddenPhysioKeys.empty();
-			
-			var activemodes = [false, false, false];
-			graph.getModels().forEach(function (model) {
-				activemodes[model.displaymode.id] = true;
-			});
-			
-			addKeyToParent(graph, $("#stagemenu #stagekey #modelkey"), NodeType.MODEL, null);
-			addKeyToParent(graph, $("#stagemenu #stagekey #modelkey"), NodeType.EXTRACTION, null);
-			
-			for (x in DisplayModes) {
-				if (DisplayModes[x] == DisplayModes.SHOWPHYSIOMAP) {
-					populateNodeKeys(graph, x, visiblePhysioKeys, hiddenPhysioKeys);
-				}
-				else {
-					populateNodeKeys(graph, x, visibleModDepKeys, hiddenModDepKeys);
-				}
-			}
-			
-			for(key in LinkDisplayModes.SUBDEP.keys) {
-				addLinkKeyToParent(graph, visibleModDepKeys, LinkDisplayModes.SUBDEP.keys[key]);
-			}
-			for(key in LinkDisplayModes.PHYSIOMAP.keys) {
-				addLinkKeyToParent(graph, visiblePhysioKeys, LinkDisplayModes.PHYSIOMAP.keys[key]);
-			}
+function KeyElement (modDepKeys, physioKeys) {
+    var addedKeys = [];
 
-		});
-	}
-	
-	var populateNodeKeys = function(graph, x, visible, hidden) {
-		DisplayModes[x].keys.forEach(function(type) {
-			if (graph.nodesVisible[type.id]) {
-				addKeyToParent(graph, visible, type, "hideNodes");
-			}
-			else {
-				addKeyToParent(graph, hidden, type, "showNodes");
-			}
-		});
-	}
-	
-	var addKeyToParent = function (graph, parentElement, keyInfo, func) {
-			if (legendContainsKey(keyInfo)) return; 
-		
-			var keyElement = document.createElement("li"),
-			slash = "";
-			$(keyElement).text(keyInfo.nodeType);
-			addedKeys.push(keyInfo.nodeType);
-			keyElement.style.color = keyInfo.color;
-			
-			if (keyInfo==NodeType.UNSPECIFIED) {
-				slash = '<line x1="18" y1="0" x2="2" y2="16" style="stroke:#000000; stroke-width:2"; />';
-			}
-			
-			
-			keyElement.innerHTML = '<svg height="16" width="200">' +
-		  		'<circle transform="translate(10,8)" r="6" style="fill:' + keyInfo.color + '; stroke: #000000;" />' + slash +
-		  		'<text x="54" y="14" stroke="' + keyInfo.nodeType + '">'+ keyInfo.nodeType + '</text>' +
-			 '</svg>';
-			
-			// Put border around "Mediator" text for consistency with node border
-			if(keyInfo.nodeType == "Mediator") {
-				keyElement.style.webkitTextStroke = ".7px black";
-			}
+    this.initialize = function (graph) {
+        $(graph).on("postupdate", function () {
+            addedKeys = [];
+            $("#stagemenu #stagekey #modelkey").empty();
+            modDepKeys.empty();
+            physioKeys.empty();
 
-			if(keyInfo.canShowHide) {
-				$(keyElement).click(function (e) {
-					graph[func]($(e.target).text());
-				});
-				
-				keyElement.className += " canClick";
-			}
-			
-			parentElement.append(keyElement);
-	}
+            var activemodes = [false, false, false];
+            graph.getModels().forEach(function (model) {
+                activemodes[model.displaymode.id] = true;
+            });
 
-	var legendContainsKey = function(key) {
-		for (x in addedKeys) {
-			if (addedKeys[x] == key.nodeType) return true;
-		}
-		return false;
-	}
-	
-	// Adds link keys to the parent element based on the link in the nodes array
-	var addLinkKeyToParent = function (graph, parentElement, keyInfo, func) {
+            addKeyToParent(graph, $("#stagemenu #stagekey #modelkey"), NodeType.MODEL, null);
+            addKeyToParent(graph, $("#stagemenu #stagekey #modelkey"), NodeType.EXTRACTION, null);
 
-		//Add all links
-			var keyElement = document.createElement('ln'), 
-				dasharray = "";
-			
-			if (keyInfo == LinkLevels.MEDIATOR ) {
-				
-				dasharray = 'stroke-dasharray: 3, 6;';
-			}
-			
-			keyElement.innerHTML = '<svg height="16" width="200">' +
-			  		'<line x1="0" y1="8" x2="50" y2="8" style="stroke:' + keyInfo.color + ';stroke-width:' + keyInfo.linewidth+ '; ' + dasharray+'" />' +
-			  		'<text x="54" y="14" fill="' + keyInfo.color + '">'+ keyInfo.text + '</text>' +
-				 '</svg>';
-			
-			
-			
-			parentElement.append(keyElement);			
+            for (x in DisplayModes) {
+                if (DisplayModes[x] == DisplayModes.SHOWPHYSIOMAP) {
+                    populateNodeKeys(graph, x, physioKeys);
+                }
+                else {
+                    populateNodeKeys(graph, x, modDepKeys);
+                }
+            }
 
-			//$(keyElement).text(keyInfo.text);
-			//keyElement.style.color = keyInfo.color;
-			if(keyInfo.canShowHide) {
-				$(keyElement).click(function (e) {
-					graph[func]($(e.target).text());
-				});
+            for(key in LinkDisplayModes.SUBDEP.keys) {
+                addLinkKeyToParent(graph, modDepKeys, LinkDisplayModes.SUBDEP.keys[key]);
+            }
+            for(key in LinkDisplayModes.PHYSIOMAP.keys) {
+                addLinkKeyToParent(graph, physioKeys, LinkDisplayModes.PHYSIOMAP.keys[key]);
+            }
 
-				keyElement.className += " canClick";
-			}
-	}
+        });
+    }
+
+    var populateNodeKeys = function(graph, x, allKeys) {
+        DisplayModes[x].keys.forEach(function(type) {
+            if (graph.nodesVisible[type.id]) {
+                addKeyToParent(graph, allKeys, type, "hideNodes");
+            }
+            else {
+                addKeyToParent(graph, allKeys, type, "showNodes");
+            }
+        });
+    }
+
+    var addKeyToParent = function (graph, parentElement, keyInfo, func) {
+        if (legendContainsKey(keyInfo)) return;
+
+        var keyElement = document.createElement("li"),
+            slash = "";
+        $(keyElement).text(keyInfo.nodeType);
+        addedKeys.push(keyInfo.nodeType);
+        keyElement.style.color = keyInfo.color;
+
+        if (keyInfo==NodeType.UNSPECIFIED) {
+            slash = '<line x1="18" y1="0" x2="2" y2="16" style="stroke:#000000; stroke-width:2"; />';
+        }
+
+        keyElement.innerHTML = '<svg height="16" width="200">' +
+            '<circle transform="translate(10,8)" r="6" style="fill:' + keyInfo.color + '; stroke: #000000;" />' + slash +
+            '<text x="54" y="14" stroke="' + keyInfo.nodeType + '">'+ keyInfo.nodeType + '</text>' +
+            '</svg>';
+
+        // Put border around "Mediator" text for consistency with node border
+        if(keyInfo.nodeType == "Mediator") {
+            keyElement.style.webkitTextStroke = ".7px black";
+        }
+
+        if(keyInfo.canShowHide) {
+            $(keyElement).click(function (e) {
+                graph[func]($(e.target).text());
+            });
+
+            keyElement.className += " canClick";
+        }
+
+        if(func == "showNodes") {
+            keyElement.className += " hiddenKeys";
+        }
+        else if (func == "hideNodes") {
+            keyElement.className += " visibleKeys";
+        }
+
+        parentElement.append(keyElement);
+    }
+
+    var legendContainsKey = function(key) {
+        for (x in addedKeys) {
+            if (addedKeys[x] == key.nodeType) return true;
+        }
+        return false;
+    }
+
+    // Adds link keys to the parent element based on the link in the nodes array
+    var addLinkKeyToParent = function (graph, parentElement, keyInfo, func) {
+
+        //Add all links
+        var keyElement = document.createElement('ln'),
+            dasharray = "";
+
+        if (keyInfo == LinkLevels.MEDIATOR ) {
+
+            dasharray = 'stroke-dasharray: 3, 6;';
+        }
+
+        keyElement.innerHTML = '<svg height="16" width="200">' +
+            '<line x1="0" y1="8" x2="50" y2="8" style="stroke:' + keyInfo.color + ';stroke-width:' + keyInfo.linewidth+ '; ' + dasharray+'" />' +
+            '<text x="54" y="14" fill="' + keyInfo.color + '">'+ keyInfo.text + '</text>' +
+            '</svg>';
+
+        parentElement.append(keyElement);
+
+        //$(keyElement).text(keyInfo.text);
+        //keyElement.style.color = keyInfo.color;
+        if(keyInfo.canShowHide) {
+            $(keyElement).click(function (e) {
+                graph[func]($(e.target).text());
+            });
+
+            keyElement.className += " canClick";
+        }
+
+        if(func == "showNodes") {
+            keyElement.className += " hiddenKeys";
+        }
+        else if (func == "hideNodes") {
+            keyElement.className += " visibleKeys";
+        }
+    }
 }
 
 KeyElement.instance;
 KeyElement.getInstance = function () {
-	return KeyElement.instance;
+    return KeyElement.instance;
 }
 
 $(window).load(function () {
-	KeyElement.instance = new KeyElement($("#stagemenu #stagekey #subdepkeys ul.visibleKeys"), $("#stagemenu #stagekey #subdepkeys ul.hiddenKeys"),
-			$("#stagemenu #stagekey #physkeys ul.visibleKeys"), $("#stagemenu #stagekey #physkeys ul.hiddenKeys"));
+    KeyElement.instance = new KeyElement($("#stagemenu #stagekey #subdepkeys ul.keys"),
+        $("#stagemenu #stagekey #physkeys ul.keys"));
 });
