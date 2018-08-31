c82e6f84416388deebfab3edfdac3703a39814b8
Maxwell Neal <maxneal@gmail.com>
2018-08-31 12:35:47 -0700

    Reduced height of model-level annotations dialog.

--- src/semgen/annotation/dialog/modelanns/ModelLevelMetadataDialog.java
+++ src/semgen/annotation/dialog/modelanns/ModelLevelMetadataDialog.java
@@ -57,7 +57,7 @@ public class ModelLevelMetadataDialog extends SemGenDialog implements PropertyCh
 		this.setModalExclusionType(Dialog.ModalExclusionType.NO_EXCLUDE);
 		
 		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-		setPreferredSize(new Dimension(1000, 900));
+		setPreferredSize(new Dimension(1000, 800));
 		setMaximumSize(getPreferredSize());
 		setMinimumSize(getPreferredSize());
 		setResizable(true);
